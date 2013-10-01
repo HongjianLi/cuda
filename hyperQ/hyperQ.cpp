9b3c4ffc898d62acaee1133c717f3366176115b1
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-10-01 11:11:36 +0800

    Added num_kernels to hyperQ example

--- hyperQ/hyperQ.cpp
+++ hyperQ/hyperQ.cpp
@@ -4,54 +4,57 @@
 int main(int argc, char *argv[])
 {
 	const int num_milliseconds = 10;
+	const int num_kernels = 2;
 	const int num_streams = 32;
 	CUdevice device;
 	CUcontext context;
 	CUmodule module;
-	CUfunction kernel0, kernel1;
+	CUfunction function;
 	CUstream *streams;
 	CUevent beg, end;
-	int clock_rate;
+	int clock_rate, cc_major, cc_minor;
 	clock_t num_clocks;
 	float elapsed;
-	int i;
+	int s, k;
 	checkCudaErrors(cuInit(0));
 	checkCudaErrors(cuDeviceGet(&device, 0));
+	checkCudaErrors(cuDeviceGetAttribute(&cc_major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, device));
+	checkCudaErrors(cuDeviceGetAttribute(&cc_minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, device));
 	checkCudaErrors(cuDeviceGetAttribute(&clock_rate, CU_DEVICE_ATTRIBUTE_CLOCK_RATE, device));
 	checkCudaErrors(cuCtxCreate(&context, CU_CTX_SCHED_AUTO, device));
 	checkCudaErrors(cuModuleLoad(&module, "hyperQ.cubin"));
-	checkCudaErrors(cuModuleGetFunction(&kernel0, module, "kernel0"));
-	checkCudaErrors(cuModuleGetFunction(&kernel1, module, "kernel1"));
+	checkCudaErrors(cuModuleGetFunction(&function, module, "hyperQ"));
 	num_clocks = clock_rate * num_milliseconds;
 	streams = (CUstream*)malloc(sizeof(CUstream) * num_streams);
-	for (i = 0; i < num_streams; ++i)
+	for (s = 0; s < num_streams; ++s)
 	{
-		checkCudaErrors(cuStreamCreate(&streams[i], CU_STREAM_DEFAULT));
+		checkCudaErrors(cuStreamCreate(&streams[s], CU_STREAM_DEFAULT));
 	}
 	checkCudaErrors(cuEventCreate(&beg, CU_EVENT_DEFAULT));
 	checkCudaErrors(cuEventCreate(&end, CU_EVENT_DEFAULT));
 	checkCudaErrors(cuEventRecord(beg, 0));
-	for (int i = 0; i < num_streams; ++i)
+	for (s = 0; s < num_streams; ++s)
 	{
-		void* args[] = { &num_clocks };
-		checkCudaErrors(cuLaunchKernel(kernel0, 1, 1, 1, 1, 1, 1, 0, streams[i], args, NULL));
-		checkCudaErrors(cuLaunchKernel(kernel1, 1, 1, 1, 1, 1, 1, 0, streams[i], args, NULL));
+		for (k = 0; k < num_kernels; ++k)
+		{
+			checkCudaErrors(cuLaunchKernel(function, 1, 1, 1, 1, 1, 1, 0, streams[s], (void*[]){ &num_clocks }, NULL));
+		}
 	}
 	checkCudaErrors(cuEventRecord(end, 0));
 	checkCudaErrors(cuEventSynchronize(end));
 	checkCudaErrors(cuEventElapsedTime(&elapsed, beg, end));
-	printf("%d streams, each %d kernels, each %d ms\n", num_streams, 2, num_milliseconds);
-	printf("       SM <= 1.3: %d ms\n", num_milliseconds * 2 * num_streams);
-	printf("2.0 <= SM <= 3.0: %d ms\n", num_milliseconds * (num_streams + 1));
-	printf("3.5 <= SM       : %d ms\n", num_milliseconds * 2);
-	printf("sample execution: %.3f ms\n", elapsed);
 	cuEventDestroy(end);
 	cuEventDestroy(beg);
-	for (int i = 0; i < num_streams; ++i)
+	for (s = 0; s < num_streams; ++s)
 	{
-		cuStreamDestroy(streams[i]);
+		cuStreamDestroy(streams[s]);
 	}
 	free(streams);
 	checkCudaErrors(cuModuleUnload(module));
 	checkCudaErrors(cuCtxDestroy(context));
+	printf("%d streams, each %d kernels, each %d ms\n", num_streams, num_kernels, num_milliseconds);
+	printf("       SM <= 1.3:%4d ms\n", num_milliseconds * num_kernels * num_streams);
+	printf("2.0 <= SM <= 3.0:%4d ms\n", num_milliseconds * (1 + (num_kernels - 1) * num_streams));
+	printf("3.5 <= SM       :%4d ms\n", num_milliseconds * num_kernels);
+	printf("       SM == %d.%d:%4d ms\n", cc_major, cc_minor, (int)elapsed);
 }
--- hyperQ/hyperQ.cu
+++ hyperQ/hyperQ.cu
@@ -1,14 +1,4 @@
-__device__ void spin(const clock_t num_clocks)
+extern "C" __global__ void hyperQ(clock_t num_clocks)
 {
 	for (const clock_t threshold = clock() + num_clocks; clock() < threshold;);
-}
-
-extern "C" __global__ void kernel0(clock_t num_clocks)
-{
-	spin(num_clocks);
-}
-
-extern "C" __global__ void kernel1(clock_t num_clocks)
-{
-	spin(num_clocks);
 }
