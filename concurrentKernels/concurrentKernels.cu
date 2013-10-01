34ad0fff4ab5a75b8d1a713b373c7586149778b8
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-10-01 10:15:09 +0800

    Added const to num_clocks

--- concurrentKernels/concurrentKernels.cu
+++ concurrentKernels/concurrentKernels.cu
@@ -1,4 +1,4 @@
-extern "C" __global__ void spin(clock_t num_clocks)
+extern "C" __global__ void spin(const clock_t num_clocks)
 {
     const clock_t threshold = clock() + num_clocks;
     while (clock() < threshold);
