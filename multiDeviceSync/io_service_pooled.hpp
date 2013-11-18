d038a4285b79704c635602ef4786bbbaaed1065d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-11-19 00:00:39 +0800

    Added multiDeviceSync

---
+++ multiDeviceSync/io_service_pooled.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef IO_SERVICE_POOLED_HPP
+#define IO_SERVICE_POOLED_HPP
+
+#include <vector>
+#include <future>
+#include <boost/asio/io_service.hpp>
+using namespace std;
+using namespace boost::asio;
+
+class io_service_pooled : public io_service
+{
+public:
+	io_service_pooled(const int concurrency);	
+	void init(const int num_tasks);	
+	void done();
+	void sync();
+private:
+	vector<future<void>> futures;
+	work w;
+	mutex m;
+	condition_variable cv;
+	size_t num_tasks;
+	size_t num_completed_tasks;
+};
+
+#endif
---
+++ multiDeviceSync/multiDevice.cpp
@@ -0,0 +1,220 @@
+#include <ctime>
+#include <boost/filesystem/operations.hpp>
+#include "../cu_helper.h"
+#include "io_service_pooled.hpp"
+using namespace std;
+using namespace boost::filesystem;
+
+void spin(const clock_t num_clocks)
+{
+	for (const clock_t threshold = clock() + num_clocks; clock() < threshold;);
+}
+
+class ligand
+{
+public:
+	ligand(const path p) : p(p)
+	{
+		spin(1e+4); // Parse file.
+	}
+
+	void populate(int* h_l)
+	{
+		spin(1e+3); // Write data.
+	}
+
+	void write() const
+	{
+		spin(1e+5);
+	}
+
+	path p;
+};
+
+template <typename T>
+class safe_vector : public vector<T>
+{
+public:
+	safe_vector(const size_t n) : vector<T>(n)
+	{
+	}
+	void safe_push_back(const T x)
+	{
+		lock_guard<mutex> guard(m);
+		this->push_back(x);
+		cv.notify_one();
+	}
+	T safe_pop_back()
+	{
+		unique_lock<mutex> lock(m);
+		if (this->empty()) cv.wait(lock);
+		const T x = this->back();
+		this->pop_back();
+		return x;
+	}
+private:
+	mutex m;
+	condition_variable cv;
+};
+
+template <typename T>
+class callback_data
+{
+public:
+	callback_data(const T dev, safe_vector<T>& idle, io_service_pooled& io) : dev(dev), idle(idle), io(io)
+	{
+	}
+
+	T dev;
+	safe_vector<T>& idle;
+	io_service_pooled& io;
+};
+
+int main(int argc, char* argv[])
+{
+	// Initialize constants.
+	const unsigned int lws = 256;
+	const unsigned int gws = 32 * lws;
+
+	// Initialize variables.
+	srand(time(0));
+	auto h_p = static_cast<float*>(malloc(sizeof(float) * 16));
+	for (int i = 0; i < 16; ++i)
+	{
+		h_p[i] = rand() / (float)RAND_MAX;
+	}
+
+	// Create a thread pool.
+	io_service_pooled io(2);
+
+	// Initialize the CUDA driver API.
+	checkCudaErrors(cuInit(0));
+
+	// Get the number of devices with compute capability 1.0 or greater that are available for execution.
+	int num_devices;
+	checkCudaErrors(cuDeviceGetCount(&num_devices));
+
+	// Initialize containers of contexts and functions.
+	vector<CUcontext> contexts(num_devices);
+	vector<CUfunction> functions(num_devices);
+	vector<int> can_map_host_memory(num_devices);
+
+	// Populate containers of contexts and functions.
+	for (int dev = 0; dev < num_devices; ++dev)
+	{
+		// Get a device handle from an ordinal.
+		CUdevice device;
+		checkCudaErrors(cuDeviceGet(&device, dev));
+
+		// Check if the device can map host memory into CUDA address space.
+		checkCudaErrors(cuDeviceGetAttribute(&can_map_host_memory[dev], CU_DEVICE_ATTRIBUTE_CAN_MAP_HOST_MEMORY, device));
+
+		// Create context, module and function.
+		checkCudaErrors(cuCtxCreate(&contexts[dev], CU_CTX_SCHED_AUTO | CU_CTX_MAP_HOST, device));
+		CUmodule module;
+		checkCudaErrors(cuModuleLoad(&module, "monte_carlo.cubin")); // nvcc -cubin -arch=sm_11 -G, and use cuda-gdb
+		checkCudaErrors(cuModuleGetFunction(&functions[dev], module, "monte_carlo"));
+
+		// Initialize symbols.
+		CUdeviceptr d_p;
+		checkCudaErrors(cuModuleGetGlobal(&d_p, NULL, module, "p"));
+		checkCudaErrors(cuMemcpyHtoD(d_p, h_p, sizeof(float) * 16));
+
+		// Pop the current context.
+		checkCudaErrors(cuCtxPopCurrent(NULL));
+	}
+
+	// Initialize a vector of idle devices.
+	safe_vector<int> idle(num_devices);
+	iota(idle.begin(), idle.end(), 0);
+
+	// Initialize a default constructed directory_iterator acts as the end iterator.
+	const directory_iterator const_dir_iter;
+	for (directory_iterator dir_iter("."); dir_iter != const_dir_iter; ++dir_iter)
+	{
+		// Parse the ligand.
+		ligand lig(dir_iter->path());
+
+		// Check for new atom types
+		if (false)
+		{
+			const size_t n = 5;
+			io.init(n);
+			for (size_t i = 0; i < n; ++i)
+			{
+				io.post([&]()
+				{
+					io.done();
+				});
+			}
+			io.sync();
+			for (auto& context : contexts)
+			{
+				checkCudaErrors(cuCtxPushCurrent(context));
+//				checkCudaErrors(cuMemAlloc(&d_e, sizeof(float) * 1e+7));
+//				checkCudaErrors(cuMemcpyHtoD(d_e, h_e, sizeof(float) * 1e+7));
+				checkCudaErrors(cuCtxPopCurrent(NULL));
+			}
+		}
+
+		io.post([&]()
+		{
+			// Wait until a device is ready for execution.
+			const int dev = idle.safe_pop_back();
+
+			checkCudaErrors(cuCtxPushCurrent(contexts[dev]));
+			float* h_l;
+			checkCudaErrors(cuMemHostAlloc((void**) &h_l, sizeof(float) * lws, CU_MEMHOSTALLOC_DEVICEMAP));
+			for (int i = 0; i < lws; ++i)
+			{
+				h_l[i] = rand() / (float) RAND_MAX;
+			}
+			CUdeviceptr d_l;
+			if (can_map_host_memory[dev])
+			{
+				checkCudaErrors(cuMemHostGetDevicePointer(&d_l, h_l, 0));
+			}
+			else
+			{
+				checkCudaErrors(cuMemAlloc(&d_l, sizeof(float) * lws));
+				checkCudaErrors(cuMemcpyHtoDAsync(d_l, h_l, sizeof(float) * lws, NULL));
+			}
+			CUdeviceptr d_s;
+			checkCudaErrors(cuMemAlloc(&d_s, sizeof(float) * gws));
+			checkCudaErrors(cuMemsetD32Async(d_s, 0, gws, NULL));
+			checkCudaErrors(cuLaunchKernel(functions[dev], 1, 1, 1, 1, 1, 1, 0, NULL, (void*[]){ &d_s, &d_l }, NULL));
+			float* h_e;
+			checkCudaErrors(cuMemHostAlloc((void**) &h_e, sizeof(float) * lws, 0));
+			checkCudaErrors(cuMemcpyDtoHAsync(h_e, d_s, sizeof(float) * lws, NULL));
+			checkCudaErrors(cuCtxSynchronize());
+			checkCudaErrors(cuMemFree(d_s));
+			if (!can_map_host_memory[dev])
+			{
+				checkCudaErrors(cuMemFree(d_l));
+			}
+			for (int i = 0; i < lws; ++i)
+			{
+				const float actual = h_e[i];
+				const float expected = h_l[i] * 2.0f + 1.0f + h_p[i % 16];
+				if (fabs(actual - expected) > 1e-7)
+				{
+					printf("h_e[%d] = %f, expected = %f\n", i, actual, expected);
+					break;
+				}
+			}
+			checkCudaErrors(cuMemFreeHost(h_l));
+			//		lig->write();
+			//		safe_printf();
+			checkCudaErrors(cuMemFreeHost(h_e));
+			checkCudaErrors(cuCtxPopCurrent(NULL));
+			idle.safe_push_back(dev);
+		});
+	}
+
+	// Cleanup.
+	for (auto& context : contexts)
+	{
+		checkCudaErrors(cuCtxDestroy(context));
+	}
+	printf("exiting\n");
+}
