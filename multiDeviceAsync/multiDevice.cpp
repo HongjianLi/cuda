b620a30f42a3696884303400450d8e4c0eaa1b51
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-12-01 10:13:23 +0800

    Revised the constructor of callback_data

--- multiDeviceAsync/multiDevice.cpp
+++ multiDeviceAsync/multiDevice.cpp
@@ -133,12 +133,12 @@ template <typename T>
 class callback_data
 {
 public:
-	callback_data(const T dev, safe_vector<T>& idle, io_service_pool& io, vector<float*>& cnfh, vector<float*>& ligh, vector<float>& prmh, safe_function& safe_print, size_t& num_ligands, ligand&& lig_) : dev(dev), idle(idle), io(io), cnfh(cnfh), ligh(ligh), prmh(prmh), lig(move(lig_), safe_print(safe_print), num_ligands(num_ligands)) {}
-	const T dev;
+	callback_data(io_service_pool& io, const T dev, vector<float*>& cnfh, vector<float*>& ligh, vector<float>& prmh, ligand&& lig_, safe_function& safe_print, size_t& num_ligands, safe_vector<T>& idle) : io(io), dev(dev), cnfh(cnfh), ligh(ligh), prmh(prmh), lig(move(lig_)), safe_print(safe_print), num_ligands(num_ligands), idle(idle) {}
 	io_service_pool& io;
-	vector<float*>& cnfh;
-	vector<float*>& ligh;
-	vector<float> & prmh;
+	const T dev;
+	const vector<float*>& cnfh;
+	const vector<float*>& ligh;
+	const vector<float> & prmh;
 	ligand lig;
 	safe_function& safe_print;
 	size_t& num_ligands;
@@ -383,7 +383,7 @@ int main(int argc, char* argv[])
 				// Signal the main thread to post another task.
 				idle.safe_push_back(dev);
 			});
-		}, new callback_data<int>(dev, io, cnfh, ligh, prmh, move(lig), safe_print, num_ligands, idle), 0));
+		}, new callback_data<int>(io, dev, cnfh, ligh, prmh, move(lig), safe_print, num_ligands, idle), 0));
 
 		// Pop the context after use.
 		checkCudaErrors(cuCtxPopCurrent(NULL));
