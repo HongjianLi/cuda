63c51b5c7a6311cb34b0360362f36ab1fc8884d3
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-12-20 17:01:56 +0800

    Increased the loop count to last for 1.58s

--- multiDeviceSync/multiDevice.cu
+++ multiDeviceSync/multiDevice.cu
@@ -6,7 +6,7 @@ extern "C" __global__ void monte_carlo(float* const s, const float* const l)
 	const int gid = blockDim.x * blockIdx.x + threadIdx.x;
 	const int lid = threadIdx.x;
 	q[lid] = l[lid];
-	for (int j = 0; j < 4e+2; ++j)
+	for (int j = 0; j < 2e+3; ++j)
 	for (int i = 0; i < 1e+3; ++i)
 	s[gid] = s[gid] + q[lid] * 2.0f + 1.0f + p[lid % 16];
 	s[gid] = 0;
