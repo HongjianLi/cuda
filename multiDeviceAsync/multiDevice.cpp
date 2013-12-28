46fd78b0c81950b907dcd464ff348671d810febb
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-12-28 14:42:26 +0800

    Replaced printf by cerr in case of validation error

--- multiDeviceAsync/multiDevice.cpp
+++ multiDeviceAsync/multiDevice.cpp
@@ -349,7 +349,7 @@ int main(int argc, char* argv[])
 					const float expected = ligh[dev][i] * 2.0f + 1.0f + prmh[i % 16];
 					if (fabs(actual - expected) > 1e-7)
 					{
-						printf("cnfh[%d] = %f, expected = %f\n", i, actual, expected);
+						cerr << "cnfh[" << i << "] = " << actual << ", expected = " << expected << endl;
 						break;
 					}
 				}
